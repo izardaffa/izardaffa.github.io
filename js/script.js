861624ec553ae0fe572fbcca030b60f6c42208a5
Izar Daffa <izardev@gmail.com>
2023-08-23 14:19:23 +0700

    dark theme revamped

--- js/script.js
+++ js/script.js
@@ -48,20 +48,24 @@ if(hour >= 18 || hour < 5) {
 }
 
 // theme
-const theme = document.querySelector('#styleTheme')
-const light = document.querySelector('#theme.light')
-const dark = document.querySelector('#theme.dark')
+const toggleBtn = document.getElementById('theme-toggle')
+const body = document.body
 
-light.onclick = (e) => {
-    light.style.display = 'none'
-    dark.style.display = 'block'
-    theme.setAttribute('href', 'css/dark.css')
-    e.preventDefault()
-}
+toggleBtn.addEventListener('click', () => {
+    body.classList.toggle('dark-theme')
+    if(body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark')
+        toggleBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>'
+    } else {
+        localStorage.setItem('theme', 'light')
+        toggleBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>'
+    }
+})
 
-dark.onclick = (e) => {
-    dark.style.display = 'none'
-    light.style.display = 'block'
-    theme.setAttribute('href', 'css/light.css')
-    e.preventDefault()
+const savedTheme = localStorage.getItem('theme')
+if(savedTheme === 'dark') {
+    body.classList.add('dark-theme')
+    toggleBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>'
+} else {
+    toggleBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>'
 }
